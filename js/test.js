64b08cba383de1271df0b362fd95637eaec71bd4
criticalcarpet <criticalcarpet.dev@gmail.com>
2025-12-14 17:03:22 +0530

    add: test interface

---
+++ js/test.js
@@ -0,0 +1,6 @@
+document.getElementById("numberTest").onclick = function () {
+  window.location.href = "test-number.html";
+};
+document.getElementById("nameTest").onclick = function () {
+  window.location.href = "test-name.html";
+};
