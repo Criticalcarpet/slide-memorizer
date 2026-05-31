70ceeb336c4d1dded3e3a767d2508e791ea8dbab
Criticalcarpet <5015156.karan@gmail.com>
2026-05-31 15:02:43 +0530

    fix: responsive row view of slides

--- script.js
+++ script.js
@@ -1,43 +1,44 @@
-const hamburger = document.getElementById('hamburger');
-const navActivator = document.getElementById('nav-activator');
-const navbar = document.getElementById('navbar');
-const overlay = document.getElementById('overlay');
+const hamburger = document.getElementById("hamburger");
+const navActivator = document.getElementById("nav-activator");
+const navbar = document.getElementById("navbar");
+const overlay = document.getElementById("overlay");
 
 function closeNav() {
-  navActivator.classList.remove('active');
-  navbar.classList.remove('active');
-  overlay.classList.remove('active');
+  navActivator.classList.remove("active");
 
-  hamburger.classList.remove('ti-x');
-  hamburger.classList.add('ti-menu-2');
+  navbar.classList.remove("active");
+  overlay.classList.remove("active");
+
+  hamburger.classList.remove("ti-x");
+  hamburger.classList.add("ti-menu-2");
 }
 
-navActivator.addEventListener('click', () => {
-  navActivator.classList.toggle('active');
+navActivator.addEventListener("click", () => {
+  navActivator.classList.toggle("active");
 
-  const isMenu = hamburger.classList.contains('ti-menu-2');
+  const isMenu = hamburger.classList.contains("ti-menu-2");
 
   hamburger.classList.replace(
-    isMenu ? 'ti-menu-2' : 'ti-x',
-    isMenu ? 'ti-x' : 'ti-menu-2'
+    isMenu ? "ti-menu-2" : "ti-x",
+    isMenu ? "ti-x" : "ti-menu-2"
   );
 
-  navbar.classList.toggle('active');
-  overlay.classList.toggle('active');
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
 });
 
-overlay.addEventListener('click', closeNav);
+overlay.addEventListener("click", closeNav);
 
 /* Dropdown */
 
-const dropBtn = document.getElementById('dropBtn');
-const dropContent = document.getElementById('dropContent');
+const dropBtn = document.getElementById("dropBtn");
+const dropContent = document.getElementById("dropContent");
 
-dropBtn.addEventListener('click', (event) => {
+dropBtn.addEventListener("click", (event) => {
   event.stopPropagation();
-  dropContent.classList.toggle('hidden');
+  dropContent.classList.toggle("hidden");
 });
 
-window.addEventListener('click', () => {
-  dropContent.classList.add('hidden');
+window.addEventListener("click", () => {
+  dropContent.classList.add("hidden");
 });
